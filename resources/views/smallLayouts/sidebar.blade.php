ed2d23062230fdad63392c9bb251c8f12b372244
Leonardo-Oliveira1 <oleonardo78@gmail.com>
2022-10-17 10:32:58 -0300

    feat: adicionando o hamburguer menu

--- resources/views/smallLayouts/sidebar.blade.php
+++ resources/views/smallLayouts/sidebar.blade.php
@@ -10,11 +10,53 @@
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
             <i class="bx bx-chevron-left bx-sm align-middle"></i>
         </a>
+
     </div>
+
 
     <div class="menu-inner-shadow"></div>
 
+
     <ul class="menu-inner py-1">
+        <li class="menu-item">
+            <!-- User -->
+        <li class="nav-item navbar-dropdown dropdown-user dropdown" style="margin-left: 10px; margin-top: 10px;">
+            <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                <div class="avatar avatar-online">
+                    <img src="{{ asset('assets/img/avatars/1.png') }}" alt class="w-px-40 h-auto rounded-circle" />
+                    <span class="fw-semibold" style="margin-left: 6px;">Nome do usuário</span>
+                </div>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end">
+                <li>
+                    <div class="d-flex dropdown-item">
+                        <div class="flex-grow-1">
+                            <span class="fw-semibold d-block">Nome do usuário</span>
+                            <small class="text-muted">Colaborador(a)</small>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="#">
+                        <i class="bx bx-user me-2"></i>
+                        <span class="align-middle">Meu perfil</span>
+                    </a>
+                </li>
+                <li>
+                    <div class="dropdown-divider"></div>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="#">
+                        <i class="bx bx-power-off me-2"></i>
+                        <span class="align-middle">Encerrar sessão</span>
+                    </a>
+
+                </li>
+            </ul>
+        </li>
+        <!--/ User -->
+        </li>
+
         <!-- Components -->
         <li class="menu-header small text-uppercase"><span class="menu-header-text">Estoque</span></li>
         <!-- Cards -->
@@ -46,4 +88,6 @@
                 <div data-i18n="Basic">Dados para a previsão de uso de reagentes</div>
             </a>
         </li>
+
+
 </aside>
