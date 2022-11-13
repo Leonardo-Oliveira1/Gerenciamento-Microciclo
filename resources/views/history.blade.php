269f6ab87d058c6ec3bc999be1ad419d13db2500
Leonardo-Oliveira1 <oleonardo78@gmail.com>
2022-11-13 10:37:25 -0300

    feat: terminando o estilo do histórico

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -18,15 +18,15 @@
 
     <div class="row">
 
-    <h4 class="fw-bold p-4">Últimas ações</h4>
+        <h3 class="fw-bold p-4">Últimas 20 ações</h3>
 
-    <?php
+        <?php
         setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
         date_default_timezone_set('America/Sao_Paulo');
-    ?>
-    @foreach ($histories as $history)
-    <p style="padding-left: 24px; font-size: 18px;"><strong>{{ strftime('%A, %d de %B', strtotime($history->created_at)) }}:</strong> {{ $history->user }} {{ $history->text }} <strong>{{ $history->units }} unidade(s) de '{{ $history->item }}</strong>'.</p>
-    @endforeach
+        ?>
+        @foreach ($histories as $history)
+        <p style="padding-left: 24px; font-size: 18px;"><strong style="color: #30907B;">{{ strftime('%A, %d de %B', strtotime($history->created_at)) }}:</strong> {{ $history->user }} <strong style="color: #233446;">{{ $history->text }} {{ $history->units }} unidade(s)</strong> de <strong style="color: #233446;">'{{ $history->item }}'</strong>.</p>
+        @endforeach
     </div>
 </div>
 @endsection
