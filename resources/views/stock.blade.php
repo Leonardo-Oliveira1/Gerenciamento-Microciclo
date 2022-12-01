060b700b5f8044aeeedcf3c0f18b1dcb1ed7cec9
Leonardo-Oliveira1 <oleonardo78@gmail.com>
2022-12-01 09:55:11 -0300

    fix(stock_blade): criando arquivo estático para o jquery

--- resources/views/stock.blade.php
+++ resources/views/stock.blade.php
@@ -1,7 +1,7 @@
 @extends('smallLayouts.core')
 
 @section('head')
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+<script src="{{ asset('js/jquery1.10.js')}}" type="text/javascript"></script>
 <script src="{{ asset('js/jquery.maskMoney.js')}}" type="text/javascript"></script>
 @endsection
 
